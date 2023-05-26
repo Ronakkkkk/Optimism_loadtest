7ac4384d4962bc73d7db23bc113a881f8ac943d1
Ronak <ronak01.raj2gmail.com>
2023-05-26 13:53:46 +0530

    contract updte

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,16 +6,30 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    address[] waveSenders;
+   
+    
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
+    struct Wave{
+        address waver;
+        string message;
+        uint256 timestamp;
+
+    }
+    Wave[] waves;
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
-        waveSenders.push(msg.sender);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+         emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves()public view returns(Wave[] memory){
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
@@ -23,7 +37,5 @@ contract WavePortal {
         return totalWaves;
     }
 
-    function getWaveSenders() public view returns(address[] memory){
-        return waveSenders;
-    }
+   
 }
