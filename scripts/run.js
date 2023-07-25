35f2891ecad85a6dfc17e51ef739574c350608e1
Ronak <ronak01.raj@gmail.com>
2023-07-26 01:54:42 +0530

    Rollup

--- scripts/run.js
+++ scripts/run.js
@@ -1,46 +1,14 @@
 const main = async () => {
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy({
-    value: hre.ethers.utils.parseEther("0.1"),
-  });
-  await waveContract.deployed();
-  console.log("Contract addy:", waveContract.address);
+const Greeter = await ethers.getContractFactory("Greeter")
+const greeter = await Greeter.attach("0x575E9B4f2c3945d7CF07cb76628d29DF471692B8")
+
+await greeter.greet()
+let tx
+
+tx = await greeter.setGreeting(`Hardhat: Hello ${new Date()}`)
+rcpt = await tx.wait()  
+await greeter.greet()
  
-  let contractBalance= await hre.ethers.provider.getBalance(waveContract.address);
-  console.log("Current balance:", hre.ethers.utils.formatEther(contractBalance));
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
-  console.log(waveCount.toNumber());
-
-  /**
-   * Let's send a few waves!
-   */
-
-
-  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-  console.log(
-    "Contract balance:",
-    hre.ethers.utils.formatEther(contractBalance)
-  );
-
-  
-
-  const waveTxn = await waveContract.wave("This is wave #1");
-  await waveTxn.wait();
-
-  const waveTxn2 = await waveContract.wave("This is wave #2");
-  await waveTxn2.wait();
-
-  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
-  console.log(
-    "Contract balance:",
-    hre.ethers.utils.formatEther(contractBalance)
-  );
-
-
-
-  let allWaves = await waveContract.getAllWaves();
-  console.log(allWaves);
 };
 
 const runMain = async () => {
